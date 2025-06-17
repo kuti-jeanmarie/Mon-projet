e706795f8438f43935a83ba818f988eafd1236e8
J-KUTI <kutijeanmarie@gmail.com>
2025-06-17 15:18:20 +0100

    ajout de php et  autre arrangement de la page

---
+++ traitement.php
@@ -0,0 +1,32 @@
+<?php
+
+try{
+    $conn = new PDO("mysql:host=localhost;dbname=hotel", "root", "");
+}
+catch(Exception $e){
+    echo ("Erreur: " .$e->getMessage());
+    exit();
+}
+
+$nom = $_POST['nom'];
+$email = $_POST['email'];
+$telephone = $_POST['telephone'];
+$date_heure_arrivee = $_POST['date_heure_arrivee'];
+$date_heure_depart = $_POST['date_heure_depart'];
+$nb_personnes = $_POST['nb_personne'];
+$type_chambre = $_POST['type_chambre'];
+
+$sql = $conn->prepare("INSERT INTO reservation(nom, email, telephone, date_heure_arrivee, date_heure_depart, nb_personne, type_chambre) VALUES (:nom, :email, :telephone, :date_heure_arrivee, :date_heure_depart, :nb_personnes, :type_chambre)");
+
+$sql->bindParam(':nom', $nom);
+$sql->bindParam(':email', $email);
+$sql->bindParam(':telephone', $telephone);
+$sql->bindParam(':date_heure_arrivee', $date_heure_arrivee);
+$sql->bindParam(':date_heure_depart', $date_heure_depart);
+$sql->bindParam(':nb_personnes', $nb_personnes);
+$sql->bindParam(':type_chambre', $type_chambre);
+
+$sql->execute();
+echo "Réservation effectué";
+
+?>
